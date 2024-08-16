b4f3f3355d2f4078091e08c60ab15e25f85a0819
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-08-17 00:54:00 +0400

    input/output

--- app.asm
+++ app.asm
@@ -2,29 +2,49 @@
 ; Author: Hamida Allahverdiyeva
 ; Date: 2024-08-15
 
-global _start
 
-section .text:
+section .text     ;code segment
+    global _start
 
-_start:
+_start:            ;user prompt
     mov eax, 0x4
     mov ebx, 1
-    mov ecx, message
+    mov ecx, userMsg
     mov edx, message_len
     int 0x80
+    
+    ;read and store the user input
+    mov	eax, 3     ;sys_read
+    mov	ebx, 2     ;
+    mov	ecx, num   ;num from input
+    mov	edx, 5     ;5 bytes (numeric, 1 for sign) of that information
+    int	0x80       ;call kernel
 
-    mov	edx, 9    ;message length
-    mov	ecx, s2   ;message to write
-    mov	ebx, 1    ;file descriptor (stdout)
-    mov	eax, 4    ;system call number (sys_write)
-    int	0x80     ;call kernel
+    ;Output the message 'The entered number is: '
+    mov eax, 0x4
+    mov ebx, 1
+    mov ecx, disMsg
+    mov edx, disMsg_len
+    int 0x80
+
+    ;Output the entered number
+    mov eax, 0x4
+    mov ebx, 1
+    mov ecx, num
+    mov edx, 5
+    int 0x80
 
     ;exit
     mov eax, 0x1
     mov ebx, 0
     int 0x80
 
-section .data:
-    message: db "Displaying 9 stars", 0xA
-    message_len equ $ - message
-    s2 times 9 db '*'
+section .data
+    userMsg: db "Please enter a number:", 0xA
+    message_len equ $ - userMsg
+    disMsg: db "You have entered: "
+    disMsg_len equ $ - disMsg
+    ; s2 times 9 db '*'
+
+section .bss     ;uninitialized data
+    num resb 5
